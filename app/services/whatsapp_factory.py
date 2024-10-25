0e1f92337fedb14adc91f71a3a6bf777b3ca5a7f
Eric Mendonca <ericmend@gmail.com>
2024-10-25 01:54:11 -0300

    fix: import waitress

--- app/services/whatsapp_factory.py
+++ app/services/whatsapp_factory.py
@@ -50,6 +50,11 @@ class WebDriverWhatsappFactory:
             options.add_argument("--no-sandbox")
             options.add_argument("--headless")
             options.add_argument("--disable-gpu")
+            options.add_argument("--disable-v8-optimizations")
+            
+            prefs = {"profile.managed_default_content_settings.images": 2}
+            options.add_experimental_option("prefs", prefs)
+
             WebDriverWhatsappFactory._driver = webdriver.Chrome(
                 service=Service(executable_path=env.PATH_SERVICE),
                 options=options,
